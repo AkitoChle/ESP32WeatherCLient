13317c535092ab97f3d85bbaa41c5ed042a619e0
DESKTOP-7SF07H9\Robocode 3 <robocodekh@gmail.com>
2025-05-24 17:52:02 +0300

    html server temperature&humidity

--- ESP32WeatherClient/src/main.cpp
+++ ESP32WeatherClient/src/main.cpp
@@ -1,7 +1,23 @@
 #include <DHT.h>
 #include <Arduino.h>
+#include <WebServer.h>
+#include <WiFi.h>
 
+const char* ssid = "Redmi Note 9 Pro";
+const char* password = "21012012";
+
+WebServer server(80);
 DHT dht(18,DHT11);
+float temp, hum;
+
+String html_page(float temperature, float humidity){
+  String html = "<!DOCTYPE html><html><head><meta http-equiv='refresh' content='5'>";
+  html += "<title>DHT11</title></head><body>";
+  html += "<h1>See Temperature and Humidity from DHT11 below</h1>";
+  html += "<p>Temperature: <strong>"+ String(temperature) + " C</strong></p>";                        
+  html += "<p>Temperature: <strong>"+ String(humidity) + " %</strong></p>";
+  html += "</body></html>";                  
+}
 
 void setup() {
   Serial.begin(115200);
@@ -9,14 +25,8 @@ void setup() {
 }
 
 void loop() {
-  delay(2000);
-  float temp = dht.readTemperature();
-  float humidity = dht.readHumidity();
 
-  Serial.print("Температура: ");
-  Serial.print(temp);
-    
-  Serial.print("Влажность: ");
-  Serial.print(humidity);
-  Serial.println(" %");
+  temp = dht.readTemperature();
+  hum = dht.readHumidity();
+
 }
